3ee5eb4dac1631349ea51e62e2b0e0f928190317
jinansh94 <33523477+jinansh94@users.noreply.github.com>
2018-09-15 22:02:27 -0400

    Initial Commit

---
+++ scripts/content.js
@@ -0,0 +1,32 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+ 
+var body = getUrlParameter('body');
+
+var clicktoggle = true;
+
+function displayhint(){
+    if(clicktoggle){
+        document.querySelector('#BodyContent').textContent = body;
+         document.querySelector('#BodyContent').style.fontSize = 2;
+        clicktoggle = !clicktoggle;
+    }
+    else{
+        document.querySelector('#BodyContent').textContent = "";
+        clicktoggle = !clicktoggle;
+    }
+}
+
+document.querySelector('#hintbutton').addEventListener('click',displayhint)
+
+
